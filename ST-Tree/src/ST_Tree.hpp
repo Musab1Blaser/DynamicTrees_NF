36f010da565c263f21087457705d6f2ce7864134
Musab1Blaser <musab16000@gmail.com>
2024-04-02 07:30:13 +0500

    More prototypes

--- ST-Tree/src/ST_Tree.hpp
+++ ST-Tree/src/ST_Tree.hpp
@@ -16,12 +16,40 @@ class ST_Tree
             int netcost;
             ST_Node *bhead, *bleft, *bright, *btail;
             ST_Node (bool ext, ST_Node* par, bool rev, int netmi, int netcst) : external{ext}, bparent{par}, reversed{rev}, netmin{netmi}, netcost{netcst} {};
-            ~ST_Node();
+            ~ST_Node();  // To implement
         };
     
-    public:
         ST_Tree(std::map<int, int>& treePar);    
         std::map<int, ST_Node*> vertices; 
         std::map<int, int> dparent;
-        ~ST_Tree();
+
+
+        // DS elementary operations
+        // Static Operations
+        ST_Node* path(int v);  // To implement
+        int head(ST_Node* p);  // To implement
+        int tail(ST_Node* p);  // To implement
+
+        int before(int v);  // To implement
+        int after(int v);  // To implement
+
+        int pcost(int v);  // To implement
+        int pmincost(ST_Node* p);  // To implement
+        
+        void pupdate(ST_Node* p, double x);  // To implement
+        void reverse(ST_Node* p);  // To implement
+
+        // Dynamic Operations
+        void construct(int v, int w, double x);  // To implement
+        std::tuple<int, int, double> destroy (ST_Node* u);  // To implement
+        
+        void rotateleft(ST_Node* v);  // To implement
+        void rotateright(ST_Node* v);  // To implement
+
+        // concatenate + split
+
+
+        ~ST_Tree(); // To implement
+
+    // public:
 };
