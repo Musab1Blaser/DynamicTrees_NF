df908123cd978b7cbebf1254c31ef8d668aa5d8b
codeme5 <ma08056@st.habib.edu.pk>
2024-04-22 10:35:19 +0500

    dinic new

--- Network_Flow/dt_nf.cpp
+++ Network_Flow/dt_nf.cpp
@@ -1,8 +1,17 @@
 #include "dt_nf.hpp"
 
- 
+void addRevEdges(std::vector<std::map<int, std::pair<int, int>>>& adj){
+    int n = adj.size();
+    for (int v = 1; v < n; v++){
+        for (const auto& [u, c]: adj[v]){
+            if (adj[u].count(v) == 0){
+                adj[u][v] = {0, 0};
+            }
+        }
+    }
+}
 
-std::vector<std::map<int, std::pair<int, int>>> adj_inv(std::vector<std::map<int, std::pair<int, int>>> adj){
+std::vector<std::map<int, std::pair<int, int>>> adj_inv(const std::vector<std::map<int, std::pair<int, int>>>& adj){
     int n = adj.size();
     std::vector<std::map<int, std::pair<int, int>>> inv(n);
     for(int v = 1; v < n; v++){
@@ -13,9 +22,85 @@ std::vector<std::map<int, std::pair<int, int>>> adj_inv(std::vector<std::map<int
     return inv;
 };
 
+
+bool BFS(int s, int t, const std::vector<std::map<int, std::pair<int, int>>>& adj)
+{
+    std::vector<int> level;
+    for (int i = 1; i < adj.size(); i++)
+        level[i] = -1;
+ 
+    level[s] = 0; // Level of source vertex
+ 
+    // Create a queue, enqueue source vertex
+    // and mark source vertex as visited here
+    // level[] array works as visited array also.
+    std::list<int> q;
+    q.push_back(s);
+    
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop_front();
+        for (auto [v, c] : adj[u]) {
+            if (level[v] < 0 && c.second < c.first) {
+                // Level of current vertex is,
+                // level of parent + 1
+                level[v] = level[u] + 1;
+                q.push_back(v);
+            }
+        }
+    }
+ 
+    // IF we can not reach to the sink we
+    // return false else true
+    return level[t] < 0 ? false : true;
+}
+
+
+
+
+int DinicMaxflow(int s, int t, std::vector<std::map<int, std::pair<int, int>>> adj)
+{
+
+    int n = adj.size(); //the number of nodes
+    addRevEdges(adj);
+    GraphManager* g = new  GraphManager(n-1);
+    ST_Tree* tree = new ST_Tree(false, n-1, 0);
+    std::vector<std::map<int, std::pair<int, int>>> inv = adj_inv(adj);
+    // Corner case
+    if (s == t)
+        return -1;
+ 
+    int total = 0; // Initialize result
+ 
+    // Augment the flow while there is path
+    // from source to sink
+    while (BFS(s, t, adj) == true) {
+        // store how many edges are visited
+        // from V { 0 to V }
+        int* start = new int[n + 1]{ 0 };
+ 
+        // while flow is not zero in graph from S to D
+        while (int flow = sendFlow(s, INT_MAX, t, start)) {
+ 
+            // Add path flow to overall flow
+            total += flow;
+        }
+       
+          // Remove allocated array
+          delete[] start;
+    }
+ 
+    // return maximum flow
+    return total;
+}
+
+
+
 int dinicMaxFlow(int s, int t, 
 std::vector<std::map<int, std::pair<int, int>>> adj) {
     int n = adj.size(); //the number of nodes
+    addRevEdges(adj);
     GraphManager* g = new  GraphManager(n-1);
     ST_Tree* tree = new ST_Tree(false, n-1, 0);
     std::vector<std::map<int, std::pair<int, int>>> inv = adj_inv(adj);
--- Network_Flow/dt_nf.hpp
+++ Network_Flow/dt_nf.hpp
@@ -3,5 +3,6 @@
 #include "../ST_Tree/src/visualise.hpp"
 #include <queue>
 #include <vector>
+#include <list>
 std::vector<std::map<int, int>> adj_inv(std::vector<std::map<int, int>> adj);
 int dinicMaxFlow(int s, int t, std::vector<std::map<int, int>> adj);
