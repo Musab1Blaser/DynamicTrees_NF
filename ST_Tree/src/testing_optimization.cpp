59d453dd7b5a3f75056c366c6c0dd6ff76232970
Musab1Blaser <musab16000@gmail.com>
2024-04-23 14:39:34 +0500

    added more test in optim_test

--- ST_Tree/src/testing_optimization.cpp
+++ ST_Tree/src/testing_optimization.cpp
@@ -6,28 +6,30 @@
 
 int main()
 {
-    int nodes = 10000;
-    ST_Tree ST = ST_Tree(false, nodes, 0);
-    ST_Tree ST_opt = ST_Tree(true, nodes, 0);
-    
-    auto timeByDefStart = std::chrono::high_resolution_clock::now();
-    for (int i = 1; i < nodes; i++)
-        ST.link(i, i+1, 2);
-    for (int i = 1; i < nodes; i++)
-        ST.cut(i);
-    auto timeByDefEnd = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> duration1 = timeByDefEnd - timeByDefStart;
-    
-    auto timeByOptStart = std::chrono::high_resolution_clock::now();
-    for (int i = 1; i < nodes; i++)
-        ST_opt.link(i, i+1, 2);
-    for (int i = 1; i < nodes; i++)
-        ST_opt.cut(i);
-    auto timeByOptEnd = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> duration2 = timeByOptEnd - timeByOptStart;
-    
-    std::cout << "input size: " << nodes << std::endl;
-    std::cout << "default time: " << duration1.count() << std::endl;
-    std::cout << "optimized time: " << duration2.count() << std::endl;
-
+    std::vector<int> test_values = {10, 100, 1000, 10000};
+    for (int nodes : test_values)
+    {
+        ST_Tree ST = ST_Tree(false, nodes, 0);
+        ST_Tree ST_opt = ST_Tree(true, nodes, 0);
+        
+        auto timeByDefStart = std::chrono::high_resolution_clock::now();
+        for (int i = 1; i < nodes; i++)
+            ST.link(i, i+1, 2);
+        for (int i = 1; i < nodes; i++)
+            ST.cut(i);
+        auto timeByDefEnd = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> duration1 = timeByDefEnd - timeByDefStart;
+        
+        auto timeByOptStart = std::chrono::high_resolution_clock::now();
+        for (int i = 1; i < nodes; i++)
+            ST_opt.link(i, i+1, 2);
+        for (int i = 1; i < nodes; i++)
+            ST_opt.cut(i);
+        auto timeByOptEnd = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> duration2 = timeByOptEnd - timeByOptStart;
+        
+        std::cout << "input size: " << nodes << std::endl;
+        std::cout << "default time: " << duration1.count() << std::endl;
+        std::cout << "optimized time: " << duration2.count() << std::endl;
+    }
 }
